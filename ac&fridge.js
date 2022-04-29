de3d45f375a50f892109f55a9db6ac5d767d2497
Shreyank01 <87267295+Shreyank01@users.noreply.github.com>
2022-04-29 09:45:00 +0530

    Add files via upload

---
+++ ac&fridge.js
@@ -0,0 +1,32 @@
+function setup() {
+    canvas = createCanvas(600,400);
+    canvas.center();
+    object_detector = ml5.objectDetector('cocossd' , modelLoaded);
+    document.getElementById("result").innerHTML = "Object Detected: Detecting...";
+}
+
+function modelLoaded() {
+    console.log("Model Loaded !");
+    status = true;
+    object_detector.detect(img , gotResult);
+}
+
+function gotResult(error , results) {
+    if(error) {
+        console.error(error);
+    }
+    else {
+        console.log(results);
+    }
+}
+
+img = "";
+status = "";
+
+function preload() {
+    img = loadImage("ac & fridge.jpg")
+}
+
+function draw() {
+    image(img ,0 ,0 ,600 ,400); 
+}
